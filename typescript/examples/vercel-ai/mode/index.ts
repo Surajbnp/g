3ccd69e89f550ca2169171cae7a15ef154434555
Surajbnp <surajguptabnp14c@gmail.com>
2025-02-10 12:13:27 +0000

    added

--- typescript/examples/vercel-ai/mode/index.ts
+++ typescript/examples/vercel-ai/mode/index.ts
@@ -1,7 +1,9 @@
 import readline from "node:readline";
+import { polygonAmoy } from "viem/chains";
 
 import { openai } from "@ai-sdk/openai";
 import { generateText } from "ai";
+import { Groq } from "groq-sdk";
 
 import { http } from "viem";
 import { createWalletClient } from "viem";
@@ -23,6 +25,9 @@ if (!privateKey) {
     throw new Error("WALLET_PRIVATE_KEY environment variable is required");
 }
 
+const groq = new Groq({
+    apiKey: "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",
+});
 // Normalize private key format
 privateKey = privateKey.startsWith("0x") ? privateKey : `0x${privateKey}`;
 
@@ -30,14 +35,25 @@ const account = privateKeyToAccount(privateKey as `0x${string}`);
 
 const walletClient = createWalletClient({
     account: account,
-    transport: http(process.env.RPC_PROVIDER_URL),
-    chain: mode,
+    transport: http("https://rpc-amoy.polygon.technology/"),
+    chain: polygonAmoy,
 });
+
+const CUSTOM_TOKEN: any = {
+    name: "Srj",
+    address: "0xf418499Fd878a2D4A59DAA779f0beB82e4050996",
+    decimals: 18,
+    symbol: "Srj",
+};
 
 (async () => {
     const tools = await getOnChainTools({
         wallet: viem(walletClient),
-        plugins: [sendETH(), erc20({ tokens: [USDC, MODE] }), kim()],
+        plugins: [
+            sendETH(),
+            erc20({ tokens: [USDC, MODE, CUSTOM_TOKEN] }),
+            kim(),
+        ],
     });
 
     const rl = readline.createInterface({
